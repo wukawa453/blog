b02616298edcd5a02f00174be7709e19735ebeed
Christopher Wu <chriswu452@gmail.com>
2015-02-17 12:10:03 -0800

    added comments and comment view to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
   Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do #nested resources
+    resources :comments
+  end
   root 'posts#index'
   get 'posts/new' => 'posts#new'
 end
