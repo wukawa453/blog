a6f9b00cb4f040126d923623e3eed5aa1bf0ac47
Christopher Wu <chriswu452@gmail.com>
2015-02-20 11:04:39 -0800

    finished layouts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217191847) do
+ActiveRecord::Schema.define(version: 20150219235923) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150217191847) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
