48917697e18cfba27bad5a1bd3d84dfc4f8610f0
Christopher Wu <chriswu452@gmail.com>
2015-02-20 11:07:59 -0800

    rolled back :name column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219235923) do
+ActiveRecord::Schema.define(version: 20150217191847) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20150219235923) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
