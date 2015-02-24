4eb03876870372b05fb366016135f19782013282
Christopher Wu <chriswu452@gmail.com>
2015-02-24 09:25:21 -0800

    reverted to admin boolean

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   before_save {self.email.downcase}
-  has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
+  has_many :posts
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :first_name, presence: true, length: {maximum: 50}
   validates :last_name, presence: true, length: {maximum: 50}
