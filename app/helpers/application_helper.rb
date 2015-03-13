11b81b9989e785b3b782cbb3672df9e9956d90f2
Christopher Wu <chriswu452@gmail.com>
2015-03-13 14:06:12 -0700

    added proper flash styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+  def flash_class(level)
+    if level == 'notice'
+      "alert-info"
+    elsif level == 'success'
+      "alert-success"
+    elsif level == 'error'
+      "alert-danger"
+    elsif level == 'alert'
+      "alert-warning"
+    else
+      nil
+    end
+  end
+
   def full_title(page_title="")
     base_title = "Leatherwood Hi-Lux Blog"
     if page_title.empty?
