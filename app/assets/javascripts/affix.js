d6bb1a1f1fdc6ceafc4b732c4245562157e719e0
Christopher Wu <chriswu452@gmail.com>
2015-04-02 16:05:10 -0700

    updated affix, main content section resizes on empty search page, overflow into footer

--- app/assets/javascripts/affix.js
+++ app/assets/javascripts/affix.js
@@ -2,16 +2,18 @@ $(function(){
 
     // name your elements here
     var stickyElement   = '.affix-sidebar',   // the element you want to make sticky
-        bottomElement   = '.bottom'; // the bottom element where you want the sticky element to stop (usually the footer)
-
+        content         = '.post-feed', // main content on page
+        bottomElement   = '.bottom', // the bottom element where you want the sticky element to stop (usually the footer)
+        navbar          = 'nav#header'
     // make sure the element exists on the page before trying to initalize
     if($( stickyElement ).length){
         $( stickyElement ).each(function(){
 
             // let's save some messy code in clean variables
             // when should we start affixing? (the amount of pixels to the top from the element)
-            var fromTop = 0, //$( this ).offset().top, don't want the search bar to go missing 
+            var fromTop = 0, //$( this ).offset().top, don't want the search bar to go missing
                 // where is the bottom of the element?
+                navbarHeight = $( navbar ).height(),
                 fromBottom = $( document ).height()-($( this ).offset().top + $( this ).outerHeight()),
                 // where should we stop? (the amount of pixels from the top where the bottom element is)
                 // also add the outer height mismatch to the height of the element to account for padding and borders
@@ -20,7 +22,7 @@ $(function(){
             // if the element doesn't need to get sticky, then skip it so it won't mess up your layout
             if( (fromBottom-stopOn) > 200 ){
                 // let's put a sticky width on the element and assign it to the top
-                $( this ).css('width', $( this ).width()).css('top', 78).css('position', '');
+                $( this ).css('width', $( this ).width()).css('top', $(navbarHeight)).css('position', '');
                 // assign the affix to the element
                 $( this ).affix({
                     offset: {
@@ -30,10 +32,16 @@ $(function(){
                         bottom: stopOn
                     }
                 // when the affix get's called then make sure the position is the default (fixed) and it's at the top
-              }).on('affix.bs.affix', function(){ $( this ).css('top', 78).css('position', ''); });
+              }).on('affix.bs.affix', function(){ $( this ).css('top', $(navbarHeight)).css('position', ''); });
             }
             // trigger the scroll event so it always activates
             $( window ).trigger('scroll');
+            $(window).resize(function() {
+                  if ( $(content).height() < $(this).height() ) {
+                    $( content ).height($(this).outerHeight() );
+                  }
+            });
+            $(window).trigger('resize');
         });
     }
 
